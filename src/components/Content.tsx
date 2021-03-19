af25517b71a85bd5b79cd61e9a968c39f67774fc
Lmacedox <lucasmacedo376@gmail.com>
2021-03-19 09:27:35 -0300

    'upfile'

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,33 @@
-export function Content() {
-  // Complete aqui
+// INTERFACE -> ESTÁ NO APP.TSX
+import { MovieProps } from "../App";
+import { MovieCard } from "./MovieCard";
+interface MoviesPropriets {
+  movieData: MovieProps[];
+  onSelectMovieId: string
+
+}
+
+export function Content({movieData, onSelectMovieId}:MoviesPropriets) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {onSelectMovieId}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movieData.map((movie) => (
+            <MovieCard
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,94 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+
+// INTERFACE -> ESTÁ NO APP.TSX
+import { GenreResponseProps } from "../App";
+
+interface GenreProps {
+  onHandleAction: (id: number) => void;
+  onGenreDate: GenreResponseProps[];
+  onIdSelect: number;
 }
+
+
+
+
+export function SideBar({ onHandleAction, onGenreDate, onIdSelect }: GenreProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {onGenreDate.map((genre) => (
+          <Button
+            id={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onHandleAction(genre.id)}
+            selected={onIdSelect === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// FORMA APRENDIDA
+// import { Button } from "./Button";
+
+// import { GenreResponseProps } from "../App";
+
+// interface SideBarProps {
+//   genres: GenreResponseProps[];
+//   selectGenreId: number;
+//   handleClickButton: (id: number) => void;
+// }
+
+// export function SideBar({
+//   genres,
+//   selectGenreId,
+//   handleClickButton,
+// }: SideBarProps) {
+//   return (
+//     <nav className="sidebar">
+//       <span>
+//         Watch<p>Me</p>
+//       </span>
+
+//       <div className="buttons-container">
+//         {genres.map((genre) => (
+//           <Button
+//             id={String(genre.id)}
+//             title={genre.title}
+//             iconName={genre.name}
+//             onClick={() => handleClickButton(genre.id)}
+//             selected={selectGenreId === genre.id}
+//           />
+//         ))}
+//       </div>
+//     </nav>
+//   );
+// }
